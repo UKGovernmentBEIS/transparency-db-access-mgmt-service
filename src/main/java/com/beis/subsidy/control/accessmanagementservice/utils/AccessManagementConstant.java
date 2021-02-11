a99bf1fa77789b3791e1c5b6ae9de56fff1bed33
chhagan19 <chhagan.sahu@cognizant.com>
2021-02-11 20:26:05 +0000

    Updated code for GA Scheme and Award count based on status

--- src/main/java/com/beis/subsidy/control/accessmanagementservice/utils/AccessManagementConstant.java
+++ src/main/java/com/beis/subsidy/control/accessmanagementservice/utils/AccessManagementConstant.java
@@ -8,17 +8,14 @@ public class AccessManagementConstant {
     public static String GA_ENCODER_ROLE="Granting Authority Encoder";
     public static String GA_ACTIVE_STATUS="Active";
     public static String GA_INACTIVE_STATUS ="Inactive";
-    public static String GA_PUBLISHED_STATUS="Published";
 
-    public static String SM_AWAITING_APPROVAL ="Awaiting approval";
-    public static String SM_DRAFT="Draft";
-    public static String SM_PUBLISHED_STATUS="Published";
-    public static String SM_DELETED="Deleted";
+    public static String SCHEME_ACTIVE ="Active";
+    public static String SCHEME_INACTIVE ="Inactive";
 
     public static String AWARD_AWAITING_APPROVAL ="Awaiting approval";
-    public static String AWARD_DRAFT="Draft";
+    public static String AWARD_REJECTED ="Rejected";
     public static String AWARD_PUBLISHED_STATUS="Published";
-    public static String AWARD_DELETED="Deleted";
+    public static String AWARD_INACTIVE ="Inactive";
 
     public static int TOP_GA_TO_DISPLAY = 5;
     public static int TOP_SM_TO_DISPLAY = 5;
