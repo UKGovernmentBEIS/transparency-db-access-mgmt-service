f6d2c82f0123b11159ef774dbcfc5821d8e2a43f
hhassancgi <hajra.hassan@cgi.com>
2024-06-19 16:09:30 +0100

    v17

---
+++ src/main/resources/db/migration/V17__AB135.sql
@@ -0,0 +1,8 @@
+ALTER TABLE IF EXISTS public.award
+    ADD COLUMN specific_policy_objective VARCHAR(1500);
+
+ALTER TABLE IF EXISTS public.subsidy_measure
+    ADD COLUMN specific_policy_objective VARCHAR(1500);
+
+ALTER TABLE IF EXISTS public.subsidy_measure_version
+     ADD COLUMN specific_policy_objective VARCHAR(1500);
